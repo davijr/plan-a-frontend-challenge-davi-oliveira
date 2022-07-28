05223ac4be70ea708843521a0eae163562414980
davijr <davijrconsultoria@gmail.com>
2022-07-28 17:32:36 -0300

    Implements movie detailing.

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { MovieInfoListComponent } from 'src/app/components/movie-info/movie-info.component';
 import { HomePage } from './home.page';
-import { MovieInfoListComponent } from 'src/app/components/movie-info-list/movie-info-list.component';
 
 @NgModule({
   imports: [
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { ApiService } from 'src/app/services/api.service';
+import { ToastService } from 'src/app/services/toast.service';
+import { AppState } from 'src/store/AppState';
+import { hide, show } from 'src/store/loading/loading.actions';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  latestMovie: any;
+
+  constructor(
+    private toast: ToastService,
+    private apiService: ApiService,
+    private store: Store<AppState>
+  ) { }
 
   ngOnInit() {
+    this.getLatestMovie(null);
+  }
+
+  getLatestMovie(event) {
+    this.store.dispatch(show());
+    this.apiService.getLatestMovie().subscribe(
+      movie => {
+        this.toast.success('Last movie up to date.');
+        this.latestMovie = movie;
+        this.store.dispatch(hide());
+        event?.target.complete();
+      },
+      error => {
+        this.toast.error('Something wrong happend.');
+        console.error(error);
+        event?.target.complete();
+      }
+    );
   }
 
 }
